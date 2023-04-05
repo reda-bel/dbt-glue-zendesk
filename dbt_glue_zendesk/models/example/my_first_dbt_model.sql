a8180d2fc739835531b25d7d1e7c050582d2adb7
reda-bel <36329830+reda-bel@users.noreply.github.com>
2023-04-05 18:35:52 +0200

    test models
    Tested and functional on local<->redshift

--- dbt_glue_zendesk/models/example/my_first_dbt_model.sql
+++ dbt_glue_zendesk/models/example/my_first_dbt_model.sql
@@ -7,14 +7,8 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='view') }}
-
 with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
+SELECT * FROM "dev"."public"."tickets" LIMIT 10
 )
 
 select *
@@ -24,4 +18,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+--where id is not null
